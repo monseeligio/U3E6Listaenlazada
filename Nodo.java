cb6a0e485e737164b76021b9acb3fbe9cf9d24cd
monseeligio <71795808+monseeligio@users.noreply.github.com>
2020-11-28 09:06:38 -0600

    Create Nodo.java

---
+++ Nodo.java
@@ -0,0 +1,13 @@
+package listasenlazadas;
+public class Nodo {
+    Object  elemento;
+    Nodo Siguiente;
+ 
+    public  Nodo (Object nombre){
+        this.elemento=nombre;
+        this.Siguiente=null; 
+        
+}
+
+    
+}
